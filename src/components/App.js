73dfc7aaf87067531369a9b809613ce39c873e59
Birajit Saikia <birajitsaikia@gmail.com>
2025-03-29 20:07:57 +0530

    my game chess

---
+++ src/components/App.js
@@ -0,0 +1,93 @@
+import '../styles/App.css';
+import { useState, useEffect } from 'react';
+import { Chessboard } from 'react-chessboard';
+import { Chess } from 'chess.js';
+
+function App() {
+  const [game, setGame] = useState(new Chess());
+  const [winner, setWinner] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function makeRandomMove() {
+    const possibleMove = game.moves();
+
+    if (game.game_over() || game.in_draw() || possibleMove.length === 0) {
+      setGameOver(true);
+      const winner = game.turn() === 'w' ? 'Black' : 'White';
+      setWinner(winner);
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * possibleMove.length);
+    safeGameMutate((game) => {
+      game.move(possibleMove[randomIndex]);
+    });
+  }
+
+  function onDrop(source, target) {
+    if (gameOver) return false;
+
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q',
+      });
+    });
+    if (move === null) return false;
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
+  function restartGame() {
+    setGame(new Chess());
+    setGameOver(false);
+    setWinner(null);
+  }
+
+  useEffect(() => {
+    function handleKeyPress(event) {
+      if (event.key === 'Enter') {
+        restartGame();
+      }
+    }
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="header">
+        <img src=
+"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png" 
+        alt="Game Image" className="game-image" />
+        <div className="game-info">
+          <h1>GeeksforGeeks Chess Game</h1>
+        </div>
+      </div>
+      <div className="chessboard-container">
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+        {gameOver && (
+          <div className="game-over">
+            <p>Game Over</p>
+            <p>Winner: {winner}</p>
+            <p>Press Enter to restart</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
